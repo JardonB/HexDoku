5493770b771a06d809d711324d5ed45507e24826
JardonB <jordan.breitling@gmail.com>
2026-01-14 08:39:57 -0700

    Moved puzzle generation away from main() as that is now handled by HexDokuDisplay

--- src/main.py
+++ src/main.py
@@ -1,16 +1,7 @@
-from board import Board
-from solver import get_unique_solution
 from display import HexDokuDisplay
 
 def main():
-    size = 16
-    solution = Board(size)
-    solution.generate_random()
-    solution.display()
-
-    puzzle = get_unique_solution(solution, percent_unfill=1)
-
-    gui = HexDokuDisplay(puzzle)
+    gui = HexDokuDisplay()
     gui.run()
 
 if __name__ == "__main__":
